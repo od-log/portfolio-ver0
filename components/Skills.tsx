344a1f5b677b59a68577be911b0ef58e0b565d24
od-log <kg-te@naver.com>
2024-06-07 04:29:34 +0900

    Add Skills component

---
+++ components/Skills.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { Button } from "./ui/moving-border";
+
+const Skills = () => {
+  const skills = ["Javascript", "ReactJS", "NextJS", "Typescript", "NodeJS"];
+  const buttonClass =
+    "bg-white dark:bg-zinc-900 text-black dark:text-white border-neutral-200 dark:border-zinc-800 text-xl";
+
+  return (
+    <div className="relative flex flex-col h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg">
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          I try to improve skillset
+        </div>
+        <div className="flex pt-8 gap-4">
+          {skills.map((skill) => (
+            <Button key={skill} borderRadius="1.75rem" className={buttonClass}>
+              {skill}
+            </Button>
+          ))}
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Skills;
