b129450ba6fe1e8b42204d2de663b9834cf93cff
od-log <kg-te@naver.com>
2024-06-10 18:46:42 +0900

    Fix css

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -18,7 +18,7 @@ export function Contact() {
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
         <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          <h1 className="text-4xl md:text-7xl font-bold text-center">
             Contact Me
           </h1>
 
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
     "bg-white dark:bg-zinc-900 text-black dark:text-white border-neutral-200 dark:border-zinc-800 text-xl";
 
   return (
-    <div className="relative flex flex-col h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg">
+    <div className="relative flex flex-col h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900 text-slate-950 transition-bg">
       <motion.div
         initial={{ opacity: 0.0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -23,7 +23,7 @@ const Skills = () => {
         <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
           I try to improve skillset
         </div>
-        <div className="flex pt-8 gap-4">
+        <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] pt-8 gap-4">
           {skills.map((skill) => (
             <Button key={skill} borderRadius="1.75rem" className={buttonClass}>
               {skill}
